92e7362bdd0dd857fd0f5958a1b00366d4849c70
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-05-14 03:01:37 -0400

    New sample unit test

--- test/kat_test.dart
+++ test/kat_test.dart
@@ -5,4 +5,13 @@ import 'package:test/test.dart';
 import 'package:fake_async/fake_async.dart';
 
 // TODO: Add tests for cat() with a non-existent file.
-void main() {}
+// Sample unit test
+int add(int a, int b) {
+  return a + b;
+}
+
+void main() {
+  test('add', () {
+    expect(add(5, 6), 11);
+  });
+}
